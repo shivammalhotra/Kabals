264d1d45944b376cf914ca40878a35b572048121
shivam <malhotra.shivam@gmail.com>
2017-12-16 15:15:23 -0600

    Add interfaces for notifications stuff

---
+++ Kabals/Notifications/ISubject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kabals.Notifications
+{
+    public interface ISubject
+    {
+        void Notify();
+        void Attach();
+        void Detach();
+    }
+}
---
+++ Kabals/Notifications/ISubscribe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kabals.Notifications
+{
+    public interface ISubscribe
+    {
+        void Update();
+    }
+}
